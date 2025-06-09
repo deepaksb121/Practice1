2e75e9a2847561c2131d925f73400e50026e82b6
deepak <sbd1211@gmail.com>
2025-06-09 15:45:39 +0530

    third commit

---
+++ src/test/java/rahulshettyacademy/practice.java
@@ -0,0 +1,28 @@
+package rahulshettyacademy;
+
+public class practice {
+
+	public static void main(String[] args) {
+		
+		webautomation();
+		apiautomation();
+		mobileautomation();
+
+	}
+	
+	public static void webautomation()
+	{
+		System.out.println("web automation");
+	}
+	
+	public static void apiautomation()
+	{
+		System.out.println("api automation");
+	}
+	
+	public static void mobileautomation()
+	{
+		System.out.println("mobile automation");
+	}
+
+}
